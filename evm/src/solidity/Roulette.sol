b465a69fe67695982c22dc6cbb0ddb74a87a49a2
Alina-rust-dev <rust.buzz@gmail.com>
2024-03-21 13:58:29 +0000

    Solidity Smart contract
    Roulette completed

--- evm/src/solidity/Roulette.sol
+++ evm/src/solidity/Roulette.sol
@@ -27,6 +27,10 @@ contract Roulette {
     /// The address of the "house" that operates this table.
     /// This is initialized to the address that deployed the contract.
     address house;
+    uint256 public constant MIN_BLOCKS_BETWEEN_SPINS = 20; // Minimum number of blocks between spins
+
+    uint256 private lastSpinBlockNumber; // Last block number when a spin occurred
+
 
     constructor() {
         house = msg.sender;
@@ -49,39 +53,79 @@ contract Roulette {
     /// accidentally depositing funds that do not back any bet.
     /// Because we have this explicit function, we do not include a fallback function.
     function fund_table() external payable {
-        //TODO
+        require(msg.sender == house, "Only the house can fund the table");
+        emit Funded(msg.value);
     }
 
     /// Place a bet on a specific color
     function place_color_bet(Color color) external payable {
-        //TODO
+        require(msg.value > 0, "Bet amount must be greater than zero");
+        color_bets.push(ColorBet(color, msg.value, msg.sender));
+        emit ColorBetPlaced(ColorBet(color, msg.value, msg.sender));
     }
 
     /// Helper function to determine the color of a roulette tile
-    function color_of(uint n) internal pure returns(Color) {
-        //TODO
+    function color_of(uint n) internal pure returns (Color) {
+        if (n == 0) {
+            return Color.Green;
+        } else if (n >= 1 && n <= 10) {
+            return (n % 2 == 0) ? Color.Black : Color.Red;
+        } else if (n >= 11 && n <= 18) {
+            return (n % 2 == 0) ? Color.Red : Color.Black;
+        } else if (n >= 19 && n <= 28) {
+            return (n % 2 == 0) ? Color.Black : Color.Red;
+        } else if (n >= 29 && n <= 36) {
+            return (n % 2 == 0) ? Color.Red : Color.Black;
+        } else {
+            revert("Invalid number");
+        }
     }
 
     /// Spin the wheel to determine the winning number.
     /// Also calls settle_bets to kick off settlement of all bets.
+    
+
     function spin() external {
-
-        // Get the "random" result and convert it into a roulette table value.
-        // This is not _quite_ perfectly fair because 2^256 % 38 != 0.
-        // We could handle that if we want, but we won't yet.
-        uint r = uint(blockhash(block.number - 1)); //FIXME we could have an interface for getting randomness and one contract assignment could be to implement a better randomness.
-        uint winning_number = r % 38;
-
+        require(color_bets.length > 0, "No bets placed yet");
+        require(block.number >= lastSpinBlockNumber + MIN_BLOCKS_BETWEEN_SPINS, "Too soon to spin again");
+        
+        uint winning_number = get_random_number();
         settle_bets(winning_number);
+        
+        lastSpinBlockNumber = block.number;
     }
+
 
     /// Helper function to settle all bets given a winning number
-    /// The payouts can be complex.
-    /// https://www.omnicalculator.com/statistics/roulette-payout
     function settle_bets(uint winning_number) internal {
-        //TODO
+        Color winning_color = color_of(winning_number);
+        for (uint i = 0; i < color_bets.length; i++) {
+            if (color_bets[i].color == winning_color) {
+                // Player wins
+                // Convert the beneficiary address to payable
+                address payable payableBeneficiary = payable(color_bets[i].beneficiary);
+
+                payableBeneficiary.transfer(color_bets[i].amount * 2); // Double the bet amount
+                emit ColorBetWon(color_bets[i]);
+            } else {
+                // Player loses
+                emit ColorBetLost(color_bets[i]);
+            }
+        }
+        delete color_bets;
+    }
+
+    /// Helper function to generate a random number between 0 and 37
+    function get_random_number() internal view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, blockhash(block.number - 1)))) % 38;
     }
 }
+
+
+
+
+    
+
 
 // Enhancement: Allow all kinds of other bets like:
 // * even / odd
